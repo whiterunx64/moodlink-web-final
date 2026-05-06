105af09f270703aee8dd760a2be042be75581df7
whiterunx64 <tadilimichael2@protonmail.com>
2026-05-06 20:54:34 +0800

    use supabase auth service, update form control, and improve auth feedback handling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,26 +14,23 @@
       Login
     </h2>
 
-    <form id="login-form" method="POST" action="#">
-      @csrf
+    <form id="login-form">
 
       <div class="space-y-4">
 
         <div>
-          <x-input-label value="Username" />
-          <x-text-input name="username" class="fade-up fade-up-3" />
-          <x-input-error :messages="$errors->get('username')" />
+          <x-input-label value="Email" />
+          <x-text-input name="email" class="fade-up fade-up-3" />
         </div>
 
         <div>
           <x-input-label value="Password" />
           <x-text-input type="password" name="password" class="fade-up fade-up-4" />
-          <x-input-error :messages="$errors->get('password')" />
         </div>
 
         <p id="js-login-error" class="text-red-600 text-xs font-semibold hidden"></p>
 
-        <x-button type="submit" class="fade-up fade-up-4 mt-2">
+        <x-button id="login-btn" type="submit" class="fade-up fade-up-4 mt-2">
           Sign In
         </x-button>
 
