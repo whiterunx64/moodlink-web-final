6c90d9e6bc890b11bca1890613267506753d3d6c
whiterunx64 <tadilimichael2@protonmail.com>
2026-05-06 21:57:25 +0800

    sync vite.config.js from development

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
@@ -8,15 +7,16 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
         }),
         tailwindcss(),
     ],
     server: {
+        host: '0.0.0.0',
+        port: 3000,
+        cors: true,
+        hmr: {
+            host: '192.168.8.106', // Example ip address host
+        },
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
